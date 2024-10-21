5271f10cc0b3d434e05be496f9fd41e6866c281a
Shubham Tiwari <shubhamtiwari1827@gmail.com>
2024-10-21 18:53:24 +0530

    change the formatting of the files

--- nvim/.config/nvim/lua/plugins/lsp_dap/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/lsp_dap/null-ls.lua
@@ -6,7 +6,9 @@ return {
 
 	config = function()
 		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+		-- Configure null-ls sources
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
@@ -14,14 +16,6 @@ return {
 				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.formatting.prettier,
 			},
-		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "format current buffer" })
-
-    -- This is the most reliable way to format on save, since it blocks editor until results are applied (or the source times out).
-		-- Auto-format on save
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-		require("null-ls").setup({
-			-- you can reuse a shared lspconfig on_attach callback here
 			on_attach = function(client, bufnr)
 				if client.supports_method("textDocument/formatting") then
 					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
@@ -29,7 +23,7 @@ return {
 						group = augroup,
 						buffer = bufnr,
 						callback = function()
-							vim.lsp.buf.formatting_sync()
+							vim.lsp.buf.format()
 						end,
 					})
 				end
--- nvim/.config/nvim/lua/plugins/lsp_dap/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/lsp_dap/treesitter.lua
@@ -1,15 +1,32 @@
 return {
-  "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufWritePre", "BufNewFile" },
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 
-  config = function()
-    local configs = require("nvim-treesitter.configs")
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      ensure_installed = "all",
-      sync_install = false,
-      auto_install = true,
-      highlight = { enable = true },
-      -- indent = { enable = true },
-    })
-  end
+		configs.setup({
+			ensure_installed = "all",
+			sync_install = false,
+			auto_install = true,
+			highlight = { enable = true },
+			-- indent = { enable = true },
+			modules = {},
+			ignore_install = {},
+
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
 }
