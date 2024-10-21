5d3747b605c76cedfe364a097f8f60125771a2fa
Shubham Tiwari <shubhamtiwari1827@gmail.com>
2024-10-21 18:51:36 +0530

    add the mappings for persistance in nvim

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -1,8 +1,22 @@
 -- Define a local map function for convenience
 local map = function(mode, lhs, rhs, opts)
-  opts = vim.tbl_extend("force", { noremap = true, silent = true }, opts or {})
-  vim.keymap.set(mode, lhs, rhs, opts)
+	opts = vim.tbl_extend("force", { noremap = true, silent = true }, opts or {})
+	vim.keymap.set(mode, lhs, rhs, opts)
 end
+
+-- save sessions with persistance
+map("n", "<leader>ss", function()
+	require("persistence").load()
+end, { desc = "load the session for the current directory" })
+map("n", "<leader>sS", function()
+	require("persistence").select()
+end, { desc = "Select the session to load" })
+map("n", "<leader>sl", function()
+	require("persistence").load({ last = true })
+end, { desc = "Load the last working session" })
+map("n", "<leader>sd", function()
+	require("persistence").stop()
+end, { desc = "stop saving sessions" })
 
 -- Buffer Navigation
 map("n", "<Tab>", "<Cmd>BufferNext<CR>", { desc = "Move to the next buffer" })
@@ -73,20 +87,20 @@ map("n", "<leader>do", "<Cmd>lua vim.lsp.buf.outgoing_calls()<CR>", { desc = "Sh
 map("n", "<leader>wa", "<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", { desc = "Add workspace folder" })
 map("n", "<leader>wr", "<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", { desc = "Remove workspace folder" })
 map(
-  "n",
-  "<leader>wl",
-  "<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-  { desc = "List workspace folders" }
+	"n",
+	"<leader>wl",
+	"<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
+	{ desc = "List workspace folders" }
 )
 
 -- code runner keymaps
-map('n', '<leader>r', ':RunCode<CR>', {desc = "Run Code"})
-map('n', '<leader>rf', ':RunFile<CR>', {desc = "Run the entire file"})
-map('n', '<leader>rft', ':RunFile tab<CR>', {desc = "Run File in new tab"})
-map('n', '<leader>rp', ':RunProject<CR>', {desc = "Run Project"})
-map('n', '<leader>rc', ':RunClose<CR>', {desc = "Close code runner"})
-map('n', '<leader>crf', ':CRFiletype<CR>', {desc = "check file type"})
-map('n', '<leader>crp', ':CRProjects<CR>', {desc = "List Projects"})
+map("n", "<leader>r", ":RunCode<CR>", { desc = "Run Code" })
+map("n", "<leader>rf", ":RunFile<CR>", { desc = "Run the entire file" })
+map("n", "<leader>rft", ":RunFile tab<CR>", { desc = "Run File in new tab" })
+map("n", "<leader>rp", ":RunProject<CR>", { desc = "Run Project" })
+map("n", "<leader>rc", ":RunClose<CR>", { desc = "Close code runner" })
+map("n", "<leader>crf", ":CRFiletype<CR>", { desc = "check file type" })
+map("n", "<leader>crp", ":CRProjects<CR>", { desc = "List Projects" })
 
 -- Telescope Key Mappings
 local builtin = require("telescope.builtin")
@@ -94,17 +108,17 @@ local builtin = require("telescope.builtin")
 -- File operations
 map("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 map("n", "<leader>fa", function()
-  builtin.find_files({ hidden = true, follow = true })
+	builtin.find_files({ hidden = true, follow = true })
 end, { desc = "Find all files (including hidden and ignored)" })
 map("n", "<leader>fA", function()
-  builtin.find_files({ no_ignore = true, hidden = true })
+	builtin.find_files({ no_ignore = true, hidden = true })
 end, { desc = "Find files including ignored" })
 map("n", "<leader>fo", builtin.oldfiles, { desc = "List recent files" })
 
 -- Live grep and search
 map("n", "<leader>fw", builtin.live_grep, { desc = "Live grep" })
 map("n", "<leader>fW", function()
-  builtin.grep_string({ search = vim.fn.input("Grep for > ") })
+	builtin.grep_string({ search = vim.fn.input("Grep for > ") })
 end, { desc = "Grep for specific string" })
 map("n", "<leader>fR", builtin.resume, { desc = "Resume last Telescope search" })
 map("n", "<leader>fz", builtin.current_buffer_fuzzy_find, { desc = "Fuzzy find in the current buffer" })
@@ -112,7 +126,7 @@ map("n", "<leader>fz", builtin.current_buffer_fuzzy_find, { desc = "Fuzzy find i
 -- Buffer management
 map("n", "<leader>fb", builtin.buffers, { desc = "List buffers" })
 map("n", "<leader>bd", function()
-  builtin.buffers({ sort_mru = true })
+	builtin.buffers({ sort_mru = true })
 end, { desc = "List buffers sorted by most recently used" })
 map("n", "<leader>fc", builtin.commands, { desc = "List commands" })
 
@@ -140,86 +154,85 @@ map("n", "<leader>-", require("oil").toggle_float, { desc = "Open parent directo
 
 -- Navigation
 map(
-  "n",
-  "]c",
-  "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'",
-  { expr = true, desc = "Jump to the next git hunk. If in diff mode, the default behavior of ']c' is used." }
+	"n",
+	"]c",
+	"&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'",
+	{ expr = true, desc = "Jump to the next git hunk. If in diff mode, the default behavior of ']c' is used." }
 )
 map(
-  "n",
-  "[c",
-  "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'",
-  { expr = true, desc = "Jump to the previous git hunk. If in diff mode, the default behavior of '[c' is used." }
+	"n",
+	"[c",
+	"&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'",
+	{ expr = true, desc = "Jump to the previous git hunk. If in diff mode, the default behavior of '[c' is used." }
 )
 
 -- Actions
 map(
-  "n",
-  "<leader>hs",
-  ":Gitsigns stage_hunk<CR>",
-  { desc = "Stage the current hunk. Can be used in normal and visual mode." }
+	"n",
+	"<leader>hs",
+	":Gitsigns stage_hunk<CR>",
+	{ desc = "Stage the current hunk. Can be used in normal and visual mode." }
 )
 map(
-  "v",
-  "<leader>hs",
-  ":Gitsigns stage_hunk<CR>",
-  { desc = "Stage the current hunk. Can be used in normal and visual mode." }
+	"v",
+	"<leader>hs",
+	":Gitsigns stage_hunk<CR>",
+	{ desc = "Stage the current hunk. Can be used in normal and visual mode." }
 )
 map(
-  "n",
-  "<leader>hr",
-  ":Gitsigns reset_hunk<CR>",
-  { desc = "Reset the current hunk. Can be used in normal and visual mode." }
+	"n",
+	"<leader>hr",
+	":Gitsigns reset_hunk<CR>",
+	{ desc = "Reset the current hunk. Can be used in normal and visual mode." }
 )
 map(
-  "v",
-  "<leader>hr",
-  ":Gitsigns reset_hunk<CR>",
-  { desc = "Reset the current hunk. Can be used in normal and visual mode." }
+	"v",
+	"<leader>hr",
+	":Gitsigns reset_hunk<CR>",
+	{ desc = "Reset the current hunk. Can be used in normal and visual mode." }
 )
 map("n", "<leader>hS", "<cmd>Gitsigns stage_buffer<CR>", { desc = "Stage the entire buffer." })
 map("n", "<leader>hu", "<cmd>Gitsigns undo_stage_hunk<CR>", { desc = "Undo the staging of the current hunk." })
 map("n", "<leader>hR", "<cmd>Gitsigns reset_buffer<CR>", { desc = "Reset the entire buffer." })
 map("n", "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "Preview the current hunk." })
 map(
-  "n",
-  "<leader>hb",
-  '<cmd>lua require"gitsigns".blame_line{full=true}<CR>',
-  { desc = "Blame the current line, showing the full blame information." }
+	"n",
+	"<leader>hb",
+	'<cmd>lua require"gitsigns".blame_line{full=true}<CR>',
+	{ desc = "Blame the current line, showing the full blame information." }
 )
 map(
-  "n",
-  "<leader>tb",
-  "<cmd>Gitsigns toggle_current_line_blame<CR>",
-  { desc = "Toggle the display of the blame information for the current line." }
+	"n",
+	"<leader>tb",
+	"<cmd>Gitsigns toggle_current_line_blame<CR>",
+	{ desc = "Toggle the display of the blame information for the current line." }
 )
 map("n", "<leader>hd", "<cmd>Gitsigns diffthis<CR>", { desc = "Show the diff of the current file." })
 map(
-  "n",
-  "<leader>hD",
-  '<cmd>lua require"gitsigns".diffthis("~")<CR>',
-  { desc = "Show the diff between the current file and the last commit." }
+	"n",
+	"<leader>hD",
+	'<cmd>lua require"gitsigns".diffthis("~")<CR>',
+	{ desc = "Show the diff between the current file and the last commit." }
 )
 map("n", "<leader>td", "<cmd>Gitsigns toggle_deleted<CR>", { desc = "Toggle the display of deleted lines." })
 
 -- Text object
 map(
-  "o",
-  "ih",
-  ":<C-U>Gitsigns select_hunk<CR>",
-  { desc = "Select the text object of the current hunk. Works in operator-pending and visual modes." }
+	"o",
+	"ih",
+	":<C-U>Gitsigns select_hunk<CR>",
+	{ desc = "Select the text object of the current hunk. Works in operator-pending and visual modes." }
 )
 map(
-  "x",
-  "ih",
-  ":<C-U>Gitsigns select_hunk<CR>",
-  { desc = "Select the text object of the current hunk. Works in operator-pending and visual modes." }
+	"x",
+	"ih",
+	":<C-U>Gitsigns select_hunk<CR>",
+	{ desc = "Select the text object of the current hunk. Works in operator-pending and visual modes." }
 )
 
 -- quickfix list and location list with git signs and trouble
 map("n", "<leader>hq", "<cmd>Gitsigns setqflist all<CR>", { desc = "Open git changes quickfix list" })
 map("n", "<leader>hl", "<cmd>Gitsigns setloclist<CR>", { desc = "Open git changes location list" })
-
 
 -- Custom Commands
 map("n", "<C-c>", "<cmd>nohlsearch<cr>", { desc = "Clear Search Highlight" })
@@ -235,12 +248,11 @@ vim.api.nvim_create_user_command("W", "w", {})
 vim.api.nvim_create_user_command("Wqall", "wqall", {})
 
 -- Remap Ctrl-h, Ctrl-j, Ctrl-k, Ctrl-l for navigation in insert mode with descriptions
-map('i', '<C-h>', '<Left>', { desc = "Move left in insert mode" })
-map('i', '<C-j>', '<Down>', { desc = "Move down in insert mode" })
-map('i', '<C-k>', '<Up>', { desc = "Move up in insert mode" })
-map('i', '<C-l>', '<Right>', { desc = "Move right in insert mode" })
+map("i", "<C-h>", "<Left>", { desc = "Move left in insert mode" })
+map("i", "<C-j>", "<Down>", { desc = "Move down in insert mode" })
+map("i", "<C-k>", "<Up>", { desc = "Move up in insert mode" })
+map("i", "<C-l>", "<Right>", { desc = "Move right in insert mode" })
 
 -- vs code like alt + j, k to move line up and down
-map('n', '<leader>j', ':m .+1<CR>==', { desc = "Move text down" })
-map('n', '<leader>k', ':m .-2<CR>==', { desc = "Move text up" })
-
+map("n", "<leader>j", ":m .+1<CR>==", { desc = "Move text down" })
+map("n", "<leader>k", ":m .-2<CR>==", { desc = "Move text up" })
