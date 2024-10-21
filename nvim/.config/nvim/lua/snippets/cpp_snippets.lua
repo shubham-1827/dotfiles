ed2e3fa1202219009f5d47cf07b7646dc85e0143
Shubham Tiwari <shubhamtiwari1827@gmail.com>
2024-10-21 18:59:21 +0530

    add the basic boilerplate of cpp to cpp_snippets

--- nvim/.config/nvim/lua/snippets/cpp_snippets.lua
+++ nvim/.config/nvim/lua/snippets/cpp_snippets.lua
@@ -4,21 +4,34 @@ local t = ls.text_node
 local i = ls.insert_node
 
 ls.add_snippets("cpp", {
-  s("#base", {
-    t({
-      "#include <iostream>",
-      "#include <string>",
-      "#include <vector>",
-      "#include <math.h>",
-      "#include <algorithm>",
-      " ",
-      "using namespace std;",
-      " ",
-      "int main() {",
-      " ",
-      "  ",
-    }),
-    i(1, "Bade bhari coder ho riya hai ... complex use karega !!!"),
-    t({ " ", " ", "  return 0;", "}" }),
-  }),
+	s("#base", {
+		t({
+			"#include <iostream>",
+			"#include <string>",
+			"#include <vector>",
+			"#include <math.h>",
+			"#include <algorithm>",
+			" ",
+			"using namespace std;",
+			" ",
+			"int main() {",
+			" ",
+			"  ",
+		}),
+		i(1, "Bade bhari coder ho riya hai ... complex use karega !!!"),
+		t({ " ", " ", "  return 0;", "}" }),
+	}),
+	s("bro", {
+		t({
+			"#include <iostream>",
+			" ",
+			"using namespace std;",
+			" ",
+			"int main() {",
+			" ",
+			"  ",
+		}),
+		i(1, "le re lund ke ..."),
+		t({ " ", " ", "  return 0;", "}" }),
+	}),
 })
