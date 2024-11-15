35c2835117edfea94d6e7f0c550f65e9c9e13f30
Shubham Tiwari <shubhamtiwari1827@gmail.com>
2024-11-15 18:26:59 +0530

    add the table for virtuual text in codeium.lua to view virtual text generated by codeium directly into the buffer

--- nvim/.config/nvim/lua/plugins/ai/codeium.lua
+++ nvim/.config/nvim/lua/plugins/ai/codeium.lua
@@ -1,12 +1,32 @@
 return {
-   "Exafunction/codeium.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "hrsh7th/nvim-cmp",
-    },
-    config = function()
-        require("codeium").setup({
-          enable_chat = true,
-        })
-    end
+	"Exafunction/codeium.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"hrsh7th/nvim-cmp",
+	},
+	config = function()
+		require("codeium").setup({
+			enable_cmp_source = false,
+			virtual_text = {
+				enabled = true,
+				key_bindings = {
+					-- Accept the current completion.
+					accept = "<C-g>",
+					-- Accept the next word.
+					accept_word = false,
+					-- Accept the next line.
+					accept_line = "<C-z>",
+					-- Clear the virtual text.
+					clear = "<C-v>",
+					-- Cycle to the next completion.
+					next = "<M-]>",
+					-- Cycle to the previous completion.
+					prev = "<M-[>",
+				},
+			},
+		})
+		require("codeium.virtual_text").set_statusbar_refresh(function()
+			require("lualine").refresh()
+		end)
+	end,
 }
